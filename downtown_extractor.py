62b0898cec06214de423f5361d141d8c72166b91
Ingo Ruhnke <grumbel@gmail.com>
2022-10-29 12:47:35 +0200

    Switch from OptionParser to ArgumentParser

--- downtown_extractor.py
+++ downtown_extractor.py
@@ -16,92 +16,97 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+from typing import Sequence
+
 import sqlite3
 import os
 import sys
-from optparse import OptionParser
+import argparse
 
-parser = OptionParser("Usage: %prog [OPTIONS] [FILES]")
-parser.add_option("-d", "--datadir", dest="datadir",
-                  help="The directory of Goin' Downtown", metavar="DIR")
-parser.add_option("-t", "--targetdir", dest="targetdir", default=".",
-                  help="The directory where files will be extracted", metavar="DIR")
-parser.add_option("-a", "--extract-all", metavar="DIR",
-                  dest="extract_all", action="store_true",
-                  help="Extract all resource files")
-parser.add_option("-s", "--stdout",
-                  dest="stdout", action="store_true",
-                  help="Extract data to stdout")
-parser.add_option("-g", "--glob", metavar="PATTERN",
-                  dest="glob_pattern",
-                  help="Select files by glob pattern")
-parser.add_option("-l", "--list",
-                  dest="list_files", action="store_true",
-                  help="List all resource files")
-parser.add_option("-e", "--extract",
-                  dest="extract_files", action="store_true",
-                  help="Extract resource files")
-parser.add_option("-r", "--resources",
-                  dest="resources", default="resources",
-                  help="Prefix of the resource files, can be 'resources' or 'german'")
 
-(options, args) = parser.parse_args()
+def parse_args(argv: Sequence[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="'Goin' Downtown' datafile extraction tool")
 
-if not options.datadir:
-    print("error: datadir not given")
-    sys.exit(1)
+    parser.add_argument("FILE", nargs='*')
+    parser.add_argument("-d", "--datadir", dest="datadir",
+                        help="The directory of Goin' Downtown", metavar="DIR")
+    parser.add_argument("-t", "--targetdir", dest="targetdir", default=".",
+                        help="The directory where files will be extracted", metavar="DIR")
+    parser.add_argument("-a", "--extract-all", dest="extract_all", action="store_true",
+                        help="Extract all resource files")
+    parser.add_argument("-s", "--stdout", dest="stdout", action="store_true",
+                        help="Extract data to stdout")
+    parser.add_argument("-g", "--glob", metavar="PATTERN", dest="glob_pattern",
+                        help="Select files by glob pattern")
+    parser.add_argument("-l", "--list", dest="list_files", action="store_true",
+                        help="List all resource files")
+    parser.add_argument("-e", "--extract", dest="extract_files", action="store_true",
+                        help="Extract resource files")
+    parser.add_argument("-r", "--resources", dest="resources", default="resources",
+                        help="Prefix of the resource files, can be 'resources' or 'german'")
 
-sql = sqlite3.connect(os.path.join(options.datadir, options.resources + '.meta'))
-cur = sql.cursor()
+    return parser.parse_args(argv[1:])
 
-if options.list_files:
-    cur.execute("select resource_id from file_information;")
-    for (resource_id,) in cur:
-        print(resource_id)
-else:
-    if not options.glob_pattern and not options.extract_all and args == []:
-        print("error: no files for extraction given")
+
+def main() -> None:
+    opts = parse_args(sys.argv)
+
+    if not opts.datadir:
+        print("error: datadir not given")
         sys.exit(1)
 
+    sql = sqlite3.connect(os.path.join(opts.datadir, opts.resources + '.meta'))
+    cur = sql.cursor()
+
+    if opts.list_files:
+        cur.execute("select resource_id from file_information;")
+        for (resource_id,) in cur:
+            print(resource_id)
     else:
-        entries = []
+        if not opts.glob_pattern and not opts.extract_all and opts.FILE == []:
+            print("error: no files for extraction given")
+            sys.exit(1)
 
-        # collect entries to extract
-        if options.extract_all:
-            cur.execute("select"
-                        "  resource_id, file_index, file_size, file_begin"
-                        "  from file_information;")
-            entries = cur.fetchall()
+        else:
+            entries = []
 
-        if options.glob_pattern:
-            cur.execute("select "
-                        "  resource_id, file_index, file_size, file_begin"
-                        "  from file_information where resource_id glob ?;", (options.glob_pattern,))
-            entries = cur.fetchall()
+            # collect entries to extract
+            if opts.extract_all:
+                cur.execute("select"
+                            "  resource_id, file_index, file_size, file_begin"
+                            "  from file_information;")
+                entries = cur.fetchall()
 
-        for resource_id in args:
-            cur.execute("select "
-                        "  resource_id, file_index, file_size, file_begin"
-                        "  from file_information where resource_id = ?;", (resource_id,))
-            entries += cur.fetchall()
+            if opts.glob_pattern:
+                cur.execute("select "
+                            "  resource_id, file_index, file_size, file_begin"
+                            "  from file_information where resource_id glob ?;", (opts.glob_pattern,))
+                entries = cur.fetchall()
 
-        # extract collected entries
-        for resource_id, file_index, file_size, file_begin in entries:
-            datafile = os.path.join(options.datadir, "%s.d%03d" % (options.resources, file_index))
-            with open(datafile, "rb") as fin:
-                fin.seek(file_begin)
-                data = fin.read(file_size)
+            for resource_id in opts.FILE:
+                cur.execute("select "
+                            "  resource_id, file_index, file_size, file_begin"
+                            "  from file_information where resource_id = ?;", (resource_id,))
+                entries += cur.fetchall()
 
-                # write the data to the target directory
-                if options.stdout:
-                    sys.stdout.buffer.write(data)
-                else:
-                    outfile = os.path.join(options.targetdir, resource_id)
-                    outdir = os.path.dirname(outfile)
-                    print("extracting \"%s\"" % outfile)
-                    if not os.path.exists(outdir):
-                        os.makedirs(outdir)
-                    with open(outfile, "wb") as fout:
-                        fout.write(data)
+            # extract collected entries
+            for resource_id, file_index, file_size, file_begin in entries:
+                datafile = os.path.join(opts.datadir, "%s.d%03d" % (opts.resources, file_index))
+                with open(datafile, "rb") as fin:
+                    fin.seek(file_begin)
+                    data = fin.read(file_size)
+
+                    # write the data to the target directory
+                    if opts.stdout:
+                        sys.stdout.buffer.write(data)
+                    else:
+                        outfile = os.path.join(opts.targetdir, resource_id)
+                        outdir = os.path.dirname(outfile)
+                        print("extracting \"%s\"" % outfile)
+                        if not os.path.exists(outdir):
+                            os.makedirs(outdir)
+                        with open(outfile, "wb") as fout:
+                            fout.write(data)
 
 # EOF #
